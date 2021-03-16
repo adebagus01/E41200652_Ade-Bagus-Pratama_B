32e163a90c90383432a59996d8a3b7344ac3754a
Ade Bagus Pratama <55870942+adebagus01@users.noreply.github.com>
2021-03-16 09:02:12 +0700

    Create Person.java

---
+++ Minggu 2/Person.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cobacoba;
+
+/**
+ *
+ * @author adeln
+ */
+public class Person {
+    
+    
+public static void main(String[] args){
+    
+    String name,stuStatus;
+    int stuId;
+    
+    name = "Lisa Palombo";
+    stuId = 123456789;
+    stuStatus = "Active";
+    
+    //mencetak input
+    System.out.println("Nama : "+name);
+    System.out.println("Student ID : "+stuId);
+    System.out.println("Student Status : "+stuStatus);
+    }
+}
+     
+      
+    
+    
+
