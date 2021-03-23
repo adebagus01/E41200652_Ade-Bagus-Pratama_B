fa514d708f3f3f1cd053fda972ef35ed7bbe25df
Ade Bagus Pratama <55870942+adebagus01@users.noreply.github.com>
2021-03-23 14:41:11 +0700

    Create Array1.java

---
+++ minggu 3/Array1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cobacoba;
+
+/**
+ *
+ * @author adeln
+ */
+public class Array1 {
+    
+    public static void main(String[] args){
+        String[] nama = {"Ade", "Bagus", "Pratama"};
+        
+        for(int i=0; i<nama.length; i++){
+            System.out.print(nama[i]);
+            System.out.print(" ");
+        }
+            
+    }
+}
